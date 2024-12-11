52859f2fbd19aa5c5ec4c7f67e5f6afed5d28f12
AlexVonex <alemilla@ucm.es>
2024-12-11 13:47:35 +0100

    Primer commit

---
+++ alexmilla-portfolio/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Hola, soy Alejandro Milla</h1>
+        <p>Desarrollador especializado en Swift, Python y JavaScript.</p>
+        <a href="#contacto" className="btn">Contáctame</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
